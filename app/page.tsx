a29577bd630479c3d9d02d206323bce628866945
Tantely <victorientina@gmail.com>
2025-04-11 15:32:37 -0400

    edit send promise in homePage

--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,22 @@ import PropertiesContainer from "@/components/home/PropertiesContainer";
 import { Suspense } from "react";
 import LoadingCards from "@/components/card/LoadingCards";
 
-export default function HomePage({
+export default async function HomePage({
   searchParams,
 }: {
   searchParams: { category?: string; search?: string };
-}) {
+  }) {
+  const searchParamsResult =  await searchParams;
   return (
     <section>
       <CategoriesList
-        category={searchParams.category}
-        search={searchParams.search}
+        category={searchParamsResult.category}
+        search={searchParamsResult.search}
       />
       <Suspense fallback={<LoadingCards />}>
         <PropertiesContainer
-          category={searchParams.category}
-          search={searchParams.search}
+          category={searchParamsResult.category}
+          search={searchParamsResult.search}
         />
       </Suspense>
     </section>
